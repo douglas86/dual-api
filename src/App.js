54e23977434936fe586a18ba1b1b271d8a19e3cd
Douglas <douglas@pop-os.localdomain>
2020-11-19 16:05:03 +0200

    Created a new component called RandomUser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './Images/logo.svg';
-import './Styles/App.css';
+import React from "react";
+import RandomUser from "./Components/RandomUser"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RandomUser />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/RandomUser.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Randomuser() {
+  return (
+    <div className="Randomuser">
+      <h1>This is a random user</h1>
+    </div>
+  );
+}
