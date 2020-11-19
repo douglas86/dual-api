3eeb97f1b5a72dfce46cc3e6e07a5d907824f081
Douglas <douglas@pop-os.localdomain>
2020-11-19 23:49:49 +0200

    randomuser api draws 10 characters to display

--- src/Components/RandomUser.jsx
+++ src/Components/RandomUser.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import User from "./User";
 
 export default function Randomuser() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?page=1&results=10&nat=us")
+      .then((response) => {
+        setUsers(response.data.results);
+      });
+  }, []);
+
   return (
     <div className="Randomuser">
       <h1>This is a random user</h1>
+      {users.map((user) => (
+        <User key={user.login.uuid} {...user} />
+      ))}
     </div>
   );
 }
