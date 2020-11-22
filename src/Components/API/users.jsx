82ba3bf0bc1402c606fcfef7108ea2555963d0a3
Douglas <douglas@pop-os.localdomain>
2020-11-22 15:45:56 +0200

    Passed users to RandomUser.jsx, cant render

---
+++ src/Components/API/users.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://randomuser.me/api/?page=1&results=1&nat=us`)
+      .then((res) => {
+        setUsers(res.data.results);
+      });
+  }, []);
+
+  return [users];
+};
+
+export default Users;
--- src/Components/RandomUser.jsx
+++ src/Components/RandomUser.jsx
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import User from "./User";
+import React from "react";
+import Users from "./API/users";
 
 export default function Randomuser() {
-  const [users, setUsers] = useState([]);
+  const [users] = Users();
 
-  useEffect(() => {
-    axios
-      .get("https://randomuser.me/api/?page=1&results=1&nat=us")
-      .then((response) => {
-        setUsers(response.data.results);
-      });
-  }, []);
-
+  console.log(users);
   return (
     <div className="Randomuser">
       <h1>This is a random user</h1>
-      {users.map((user) => (
-        <User key={user.login.uuid} {...user} />
-      ))}
+      <ul>
+        {users.map((user) => {
+          <li key={user.login.uuid}>
+            <h3>{user.name.first}</h3>
+            {console.log(`first: ${user.name.first}`)}
+          </li>;
+        })}
+      </ul>
     </div>
   );
 }
