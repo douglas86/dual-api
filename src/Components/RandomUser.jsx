78feb1bac0f0fcdf23740eeb648c05e80e79a681
Douglas <douglas@pop-os.localdomain>
2020-11-23 10:21:57 +0200

    Got the data to render and store in a city array

--- src/Components/RandomUser.jsx
+++ src/Components/RandomUser.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import Users from "./API/users";
 
 export default function Randomuser() {
+  // array from users api
   const [users] = Users();
+  // names of all cities in the users api
+  const city = [];
 
-  console.log(users);
+  // pushes all city names to the city array
+  for (let i = 0; i < users.length; i++) {
+    city.push(users[i].location.city);
+  }
+
+  console.log(city);
+
   return (
     <div className="Randomuser">
       <h1>This is a random user</h1>
       <ul>
-        {users.map((user) => {
+        {users.map((user) => (
           <li key={user.login.uuid}>
             <h3>{user.name.first}</h3>
-            {console.log(`first: ${user.name.first}`)}
-          </li>;
-        })}
+            {console.log(user.location.city)}
+          </li>
+        ))}
       </ul>
     </div>
   );
