048ec4b332ebea1ed0f21e58014ea9facaf8d9eb
Douglas <douglas@pop-os.localdomain>
2020-12-02 09:36:25 +0200

    Created RandomUser inside Test Directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RandomUser from "./Components/Test/RandomUser";
-import Weather from "./Components/Weather";
+// import Weather from "./Components/Weather";
 
 const App = () => {
   return (
---
+++ src/Components/Test/RandomUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RandomUser = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
+};
+
+export default RandomUser;
